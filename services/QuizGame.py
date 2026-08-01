aec8f05c0ed9909a6875c70f8f1e923a250fe376
nengrafi <nangrapigood@gmail.com>
2026-08-01 22:35:11 +0900

    refactor quiz_game

--- services/QuizGame.py
+++ services/QuizGame.py
@@ -1,9 +1,24 @@
 from services.save import save,load
 from services.exception import input_str,input_number
 from models.quiz import Quiz
+import json
 class QuizGame:
     def __init__(self,data:dict):
         self.data = data
+
+    def print_menu(self):
+        menu = """
+        ==================================================================
+        퀴즈 게임
+        ==================================================================
+        1. 퀴즈 풀기
+        2. 퀴즈 추가
+        3. 퀴즈 목록
+        4. 점수 확인
+        5. 종료
+        ==================================================================
+        """
+        print(menu)
 
     def quiz_add(self):
         print("새로운 퀴즈를 추가합니다! \n")
@@ -44,5 +59,27 @@ class QuizGame:
             correct = quiz.Correct()
             if correct : score += 1
 
-        if max_score < score * 100 / len(self.data["quiz"]):
+        if max_score == None:
             self.data["max_score"] = score * 100 / len(self.data["quiz"])
+
+        elif max_score < score * 100 / len(self.data["quiz"]):
+            self.data["max_score"] = score * 100 / len(self.data["quiz"])
+
+    def load():
+        try:
+            with open("data/data.json","r",encoding="utf-8") as f:
+                return json.load(f)
+
+        except FileNotFoundError:
+            print("파일이 존재하지 않습니다. 기본 데이터를 사용합니다.\n")
+
+        except json.JSONDecodeError:
+            print("파일이 손상되었습니다. 기본 데이터를 사용합니다.\n")
+
+        with open("data/example.json","r",encoding="utf-8") as f:
+            return json.load(f)
+
+    def save(data):
+        #ensure_ascii는 사람이 읽기 불가능 하게 하는것, indent로 4칸 들여쓰기 
+        with open("data/data.json","w",encoding="utf-8") as f:
+            json.dump(data,f,ensure_ascii=False,indent=4)
